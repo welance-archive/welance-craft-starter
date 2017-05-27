b6098b52e81c69eac5429aa30cade8aa5f49cbad
Enrico Icardi <enrico.icardi@gmail.com>
2017-05-27 20:16:18 +0200

    :nerd_face: 1st setup for a webpack front-end worklow! (still needs documentation in README.md)

---
+++ __src/js/main.js
@@ -0,0 +1,11 @@
+// import jquery from node_modules
+import $ from 'jquery';
+// import specific JS for the templates
+import '../../_entries/pages/page.js';
+import '../../_entries/posts/post.js';
+
+$(function() {
+    console.info( "Good News: Jquery is ready in jqueryReady!" );
+});
+
+console.info("Good News: Main.js file has been imported!");
