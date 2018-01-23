cd40faf106954df3a3048c1cfed848d41a3cfe3d
Andrea Giacobino <no.andrea@gmail.com>
2018-01-23 20:06:35 +0100

    feat(pgsql): support postgresql in php scripts that check connection and craft installation at startup
    see #42

--- docker/craft/scripts/poll-db.php
+++ docker/craft/scripts/poll-db.php
@@ -2,33 +2,10 @@
 // usage php poll-db.php 
 // read paramters from environment variables
 
-function getConnection(){
-	// 	get the parameters
-		$environment = getenv("ENVIRONMENT");
-	$db_type  = getenv("DB_DRIVER");
-	$server   = getenv("DB_SERVER");
-	$database = getenv("DB_DATABASE");
-	$username = getenv("DB_USER");
-	$password = getenv("DB_PASSWORD");
+require_once("poll-helpers.php");
 	
-	$dbUri = "$db_type://$server/$database;user=$username;password=*****  ... ";
-	// 	TODO handle postgres
-			// 	if($db_type == 'pgsql'){
-		// 		$schema = getenv("DB_SCHEMA");
-		//}		echo "testing connection [$environment]: $dbUri";
-		
-		// 		test connection
-		$dbConnection = false;
-		if($db_type == "mysql") {
-			$dbConnection = @mysqli_connect($server, $username, $password);
-		}
-		// 		TODO add postgres configuration
-		
-		return $dbConnection;
-	}
-	
-	if (getConnection() === false) {
-		exit(1);
-	}
-	exit(0);
-	?>
+if (getConnection() === false) {
+	exit(1);
+}
+exit(0);
+?>
