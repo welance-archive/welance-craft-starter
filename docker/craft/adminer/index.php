dd2c1417b9be95f4be11728e1e13137953af1d64
Andrea Giacobino <no.andrea@gmail.com>
2018-02-01 22:06:57 +0100

    feat(adminer): update adminer and adminer experience
    + default importServerPath set to /data/craft/config/database-seed.sql
    ~ login form is already filled with the correct values
    ~ the title of adminer page is set to be "Welance: {Project Name} DB"
    
    close #46

--- docker/craft/adminer/index.php
+++ docker/craft/adminer/index.php
@@ -1,23 +1,52 @@
 <?php
-function adminer_object() {
-  
-  class AdminerSoftware extends Adminer {
-    
-    function name() {
-      // custom name in title and heading
-      return 'Welance CraftCMS';
-    }
-    
-    
-    function database() {
-      // database name, will be escaped by Adminer
-      return 'craft';
+
+function adminer_object()
+{
+
+    class AdminerSoftware extends Adminer
+    {
+
+        function name()
+        {
+            // custom name in title and heading
+            return 'Welance: '.getenv('CRAFT_SITENAME').' DB';
+        }
+
+        function importServerPath()
+        {
+            $path = realpath("/data/craft/config/database-seed.sql");
+            return $path;
+        }
+
+        function loginForm()
+        {
+            global $drivers;
+
+            // this is a bit weird but neverthe less
+            $driver = getenv("DB_DRIVER") == "mysql" ? "server" : getenv("DB_DRIVER");
+
+            echo ('<input type="hidden" name="auth[driver]" value="' . $driver . '"></input>');
+            echo ('<input type="hidden" name="auth[server]" value="' . getenv("DB_SERVER") . '"></input>');
+            echo ('<input type="hidden" name="auth[username]" value="' . getenv("DB_USER") . '"></input>');
+            echo ('<input type="hidden" name="auth[password]" value="' . getenv("DB_PASSWORD") . '"></input>');
+            echo ('<input type="hidden" name="auth[db]" value="' . getenv("DB_DATABASE") . '"></input>');
+
+            echo ('<table cellspacing="0">');
+            echo ("<tr><th>" . lang('System') . "</th><td>" . getenv("DB_DRIVER") . "</td></tr>");
+            echo ("<tr><th>" . lang('Server') . "</th><td>" . getenv("DB_SERVER") . "</td></tr>");
+            echo ("<tr><th>" . lang('Username') . "</th><td>" . getenv("DB_USER") . "</td></tr>");
+            echo ("<tr><th>" . lang('Password') . "</th><td>************</td></tr>");
+            echo ("<tr><th>" . lang('Database') . "</th><td>" . getenv("DB_DATABASE") . "</td></tr>");
+            echo ('</table>');
+
+            echo script("focus(qs('#username'));");
+            echo "<p><input type='submit' value='" . lang('Login') . "'>\n";
+            echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n";
+        }
+
     }
 
-
- }
-  
-  return new AdminerSoftware;
+    return new AdminerSoftware;
 }
 
-include "./adminer-4.3.1.php";
+include "./adminer-4.5.0-en.php";
